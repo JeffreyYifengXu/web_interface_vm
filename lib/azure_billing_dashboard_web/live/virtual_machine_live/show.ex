8cabbbaedde7b3da5dcbac72c51f850c678f7f44
Paul <pkar8013@uni.sydney.edu.au>
2022-08-31 21:10:12 +1000

    Added basic graph to a VM's details page

--- lib/azure_billing_dashboard_web/live/virtual_machine_live/show.ex
+++ lib/azure_billing_dashboard_web/live/virtual_machine_live/show.ex
@@ -3,8 +3,12 @@ defmodule AzureBillingDashboardWeb.VirtualMachineLive.Show do
 
   alias AzureBillingDashboard.List_VMs
 
+  alias Contex.{BarChart, Plot, Dataset, PointPlot}
+
   @impl true
   def mount(_params, _session, socket) do
+    socket =
+      socket
     {:ok, socket}
   end
 
@@ -13,9 +17,30 @@ defmodule AzureBillingDashboardWeb.VirtualMachineLive.Show do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:virtual_machine, List_VMs.get_virtual_machine!(id))}
+     |> assign(:virtual_machine, List_VMs.get_virtual_machine!(id))
+     |> assign(:graph, test_plot())
+    }
   end
 
   defp page_title(:show), do: "Show Virtual machine"
   defp page_title(:edit), do: "Edit Virtual machine"
+
+  def test_plot() do
+    data = [{"Jan", 250}, {"Feb", 50}, {"Mar", 301}, {"Apr", 267}, {"May", 200}]
+    ds = Dataset.new(data, ["x", "y"])
+
+    options = [
+      type: :stacked,
+      orientation: :vertical,
+      phx_event_handler: "chart1_bar_clicked",
+    ]
+
+    plot = Plot.new(ds, BarChart, 500, 400, options)
+      |> Plot.titles("VM Costs over time", "")
+      |> Plot.axis_labels("Month", "Cost (Dollars)")
+
+      Plot.to_svg(plot)
+  end
+
+
 end
