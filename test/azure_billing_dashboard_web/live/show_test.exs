952b480b27e85cfffd4f10716b4fb8e09ea85452
Aidan Musa <amus4342@uni.sydney.edu.au>
2022-09-04 18:06:09 +1000

    fixed tests, 1 still failing

---
+++ test/azure_billing_dashboard_web/live/show_test.exs
@@ -0,0 +1,10 @@
+defmodule ShowTest do
+    alias AzureBillingDashboard.List_VMs
+
+    use AzureBillingDashboardWeb.ConnCase
+
+    import Phoenix.LiveViewTest
+    import AzureBillingDashboard.List_VMsFixtures
+
+
+end
