afb74e9353c24c130c51a6e340155a5d06ce25ef
Manan Vora <manavora@gmail.com>
2022-09-09 14:46:19 +1000

    Added API calls to list_v_ms_test.exs

--- lib/azure_billing_dashboard_web/controllers/page_controller.ex
+++ lib/azure_billing_dashboard_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule AzureBillingDashboardWeb.PageController do
   use AzureBillingDashboardWeb, :controller
 
   def index(conn, _params) do
+
+    # Place API Calls Here
+
     render(conn, "index.html")
   end
 end
